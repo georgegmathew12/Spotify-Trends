67dc216fea9a33ce19503f86814df819dbe07cd3
George Mathew <georgegmathew12@gmail.com>
2025-01-05 13:31:04 +0530

    Display user info

--- src/data_collection.py
+++ src/data_collection.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 from config import Config
 import os
 
-scope = 'user-library-read'
+scope = "user-library-read user-top-read user-follow-read user-read-private playlist-read-private"
 client_id = os.getenv('CID')
 client_secret = os.getenv('SECRET')
 redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
@@ -42,3 +42,46 @@ def get_user_saved_tracks(limit=20):
         for item in results['items']
     ]
     return tracks
+
+def get_user_info():
+    """ Get user's information"""
+    user = sp.current_user()
+    user_profile = {
+        'display_name':user['display_name'],
+        'profile_image':user['images'][0]['url'] if user.get('images') else None,
+        'follower_count':user['followers']['total'],
+    }
+    top_tracks = get_user_top_tracks()
+    top_artists = get_user_top_artists()
+    user_info = {
+        'user':user_profile,
+        'top_tracks':top_tracks,
+        'top_artists':top_artists
+    }
+    return user_info
+
+def get_user_top_tracks(limit=20):
+    """Get user's top tracks"""
+    results = sp.current_user_top_tracks(limit=limit)
+    top_tracks = [
+        {
+            'name': item['name'],
+            'artist': item['artists'][0]['name'],
+            'album': item['album']['name']
+        }
+        for item in results['items']
+    ]
+    return top_tracks
+
+def get_user_top_artists(limit=20):
+    """Get user's top artists"""
+    results = sp.current_user_top_artists(limit=limit)
+    top_artists = [
+        {
+            'name': item['name'],
+            'genres': item['genres'][:3],
+            'popularity': item['popularity']
+        }
+        for item in results['items']
+    ]
+    return top_artists
